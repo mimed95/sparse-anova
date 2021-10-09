0990aae1058321e27831d1aed39be607528e3b36
Michael <52825236+mimed95@users.noreply.github.com>
2021-10-09 18:12:01 +0200

    added parent class Option, child class EuropeanOption

--- src/option.py
+++ src/option.py
@@ -5,8 +5,60 @@ from scipy.stats import norm
 
 
 @dataclass()
-class AsianOption:
-    
+class Option:
+    S_0: float = 100
+    K: float = 100
+    r: float = 0.1
+    T: float = 1
+    sigma: float = 0.2
+    epsilon: float = 1e-10
+    d: int = 16
+
+    def __post_init__(self):
+        self.t_v = np.linspace(0, 1, self.d, endpoint=False) + 1/self.d
+        self.delta_t = self.t_v[0]
+        # random walk brownian
+        self.A = np.sqrt(self.delta_t)*np.tril(np.ones(self.d))
+
+    def brownian_bridge_generator(self):
+        st = np.mgrid[1:self.d+1, 1:self.d+1]/self.d
+        cov = st.min(axis=0)*self.sigma**2
+        return np.linalg.cholesky(cov)
+
+
+@dataclass
+class EuropeanOption(Option):
+    S_0: float = 100
+    K: float = 100
+    r: float = 0.1
+    sigma: float = 0.2
+    epsilon: float = 1e-10
+    d: int = 16
+
+    def S_t(self, x: np.ndarray):
+        """Calculate the asset price in the Black-Scholes model.
+
+        Args:
+            x (np.ndarray): Array with values in (0,1)
+
+        Returns:
+            np.ndarray: Asset price evolution.
+        """
+        S = self.S_0*np.exp(
+                    (self.r-0.5*self.sigma**2)*self.T
+                    + self.sigma*np.sqrt(self.T).dot(norm.ppf(x))
+        )
+        return S
+
+    def payout_func(self):
+        payout = np.maximum(
+            0, self.S_t(self.T) - self.K
+        )
+        return payout
+
+
+@dataclass()
+class AsianOption(Option):
     S_0: float = 100
     K: float = 100
     r: float = 0.1
@@ -20,16 +72,17 @@ class AsianOption:
         self.t_v = np.linspace(0, 1, self.d, endpoint=False) + 1/self.d
         self.delta_t = self.t_v[0]
         # random walk brownian
-        self.A = np.sqrt(self.delta_t)* np.tril(np.ones(self.d)) 
+        self.A = np.sqrt(self.delta_t)*np.tril(np.ones(self.d))
+
+    def S_t(self, x: np.ndarray):
+        return self.S_0*np.exp(
+            (self.r+0.5*self.sigma**2)*self.t_v.reshape(self.d, 1)
+            + self.sigma*self.A.dot(norm.ppf(x))
+        )
 
     def payout_func(self, x: np.ndarray):
-        payout = np.maximum(0, 
-            (1/self.d)*np.sum(
-                self.S_0*np.exp(
-                    (self.r+0.5*self.sigma**2)*self.t_v.reshape(self.d,1)
-                    + self.sigma*self.A.dot(norm.ppf(x)) 
-                ) - self.K, axis=0
-            )
+        payout = np.maximum(
+            0, (1/self.d)*np.sum(self.S_t(x), axis=0) - self.K
         )
         return payout
 
@@ -49,8 +102,11 @@ class AsianOption:
             such that ``abscissas.shape == (len(dist), len(weights))``.
         """
         iid_dist = cp.Iid(
-            cp.Uniform(lb+self.epsilon, ub-self.epsilon), self.d
-        ) 
+            cp.Uniform(
+                lb+self.epsilon,
+                ub-self.epsilon
+            ), self.d
+        )
         return cp.generate_quadrature(
             self.level,
             iid_dist,
